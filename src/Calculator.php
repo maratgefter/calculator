bf3ebcacffd6cf83d4b3a9ca830e5ab2aa8436ad
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-10-01 21:05:36 +0300

    tests

--- src/Calculator.php
+++ src/Calculator.php
@@ -21,4 +21,51 @@ class Calculator
         return pow($base, $exp);
     }
 
+    public function minus($a, $b)
+    {
+        return $a-$b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a*$b;
+    }
+
+    public function split_up($a, $b)
+    {
+        return $a/$b;
+    }
+
+    public function percent($a, $b)
+    {
+        return $a%$b;
+    }
+
+    public function equally($a, $b)
+    {
+        if ($a == $b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function more($a, $b)
+    {
+        if ($a > $b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function less($a, $b)
+    {
+        if ($a < $b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -29,6 +29,51 @@ class CalculatorTest extends TestCase
             'wrong pow'
         );
 
+        $this->assertEquals(
+            27,
+            $this->calc->minus(30, 3),
+            'wrong minus'
+        );
+
+        $this->assertEquals(
+            90,
+            $this->calc->multiply(30, 3),
+            'wrong multiply'
+        );
+
+        $this->assertEquals(
+            10,
+            $this->calc->split_up(30, 3),
+            'wrong split_up'
+        );
+
+        $this->assertEquals(
+            1,
+            $this->calc->percent(31, 3),
+            'wrong percent'
+        );
+
+        $this->assertIsInt(
+            $this->calc->split_up(30, 3),
+            'wrong split_up'
+        );
+
+        $this->assertTrue(
+            $this->calc->equally(3, 3),
+            'wrong equally'
+        );
+
+        $this->assertTrue(
+            $this->calc->more(4, 3),
+            'wrong more'
+        );
+
+        $this->assertTrue(
+            $this->calc->less(2, 3),
+            'wrong less'
+        );
+        
+
     }
 
     protected function tearDown(): void
